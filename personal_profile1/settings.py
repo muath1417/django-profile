08798432e01b71e85577e169dc0111f39c93c02b
muathbvb <muathoudah@gmail.com>
2020-05-11 15:43:35 +0300

    good

--- personal_profile1/settings.py
+++ personal_profile1/settings.py
@@ -126,6 +126,10 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+
+
+try:
+    from .local_settings import *
+except INSTALLED_APPS:
+    print("looks there is no file")
